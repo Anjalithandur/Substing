eef812bf23f9a53fa260623c4e7722c68111b48c
Anjalithandur <anjalithandur@gmail.com>
2025-01-30 19:35:38 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,9 @@
+public class substring {
+    public static void main(String[] args) {
+        String str="Hello World";
+        String substring=str.substring(5);
+        System.out.println("Substring:"+substring);
+    }
+
+
+}
